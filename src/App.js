31293693af9820fd402183908e60187e112fbac8
Nathan Harris <nate@natepharris.me>
2018-10-31 17:17:12 -0600

    chore(repo): Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import colleges from './colleges';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.url = "https://s3.amazonaws.com/collegevine-assets/schools/$1-thumb.jpg";
+    this.state = {
+      incorrectCollegeImages: [],
+    }
+  }
+
+  toggleIncorrectImage(id) {
+    if (this.state.incorrectCollegeImages.indexOf(id) >= 0) {
+      this.setState({ incorrectCollegeImages: this.state.incorrectCollegeImages.filter((imgId) => imgId !== id)});
+    } else {
+      this.setState({incorrectCollegeImages: [...this.state.incorrectCollegeImages, id]});
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <span>
+        {colleges.map((value, index) => {
+          return <div className={this.state.incorrectCollegeImages.indexOf(value.id) >= 0 ? 'college-block incorrect-image': 'college-block'}>
+              <img src={this.url.replace('$1', value.id)} />
+              <p>{value.college}</p>
+              <input type="checkbox" onClick={() => this.toggleIncorrectImage(value.id)} /> Incorrect Image
+          </div>
+        })}
+      </span>
     );
   }
 }
